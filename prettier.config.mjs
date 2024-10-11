af6da47be716cd92d3475810cb80a962306ce00b
りん <31785428+rin-senpai@users.noreply.github.com>
2024-10-11 23:35:23 +1100

    Setup Prettier for linting

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+	useTabs: true,
+	semi: false,
+	printWidth: 222,
+	experimentalTernaries: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: "none",
+	bracketSpacing: true,
+};
+
+export default config;
