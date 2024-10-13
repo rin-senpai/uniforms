d1fab80d789063fbf356e90dacd33a03056a248b
camellionn <thanhtra2004@gmail.com>
2024-10-13 17:03:06 +1100

    Linted

--- client/src/components/Counter.tsx
+++ client/src/components/Counter.tsx
@@ -1,40 +1,33 @@
-import { createSignal } from 'solid-js';
-import { Button } from "~/components/ui/button";
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogTitle,
-  AlertDialogTrigger
-} from "~/components/ui/alert-dialog";
+import { createSignal } from 'solid-js'
+import { Button } from '~/components/ui/button'
+import { AlertDialog, AlertDialogContent, AlertDialogDescription, AlertDialogTitle, AlertDialogTrigger } from '~/components/ui/alert-dialog'
 
 export default function Counter() {
-  const [count, setCount] = createSignal(0);
-  const [open, setOpen] = createSignal(false); // Signal to control AlertDialog open state
+	const [count, setCount] = createSignal(0)
+	const [open, setOpen] = createSignal(false) // Signal to control AlertDialog open state
 
-  return (
-    <div>
-      {/* Test Button */}
-      <Button variant="ghost" onClick={() => setCount(count() + 1)}>
-        Clicks: {count()}
-      </Button> 
+	return (
+		<div>
+			{/* Test Button */}
+			<Button variant='ghost' onClick={() => setCount(count() + 1)}>
+				Clicks: {count()}
+			</Button>
 
-      {/* Test Alert Dialog */}
-      <Button variant="destructive" onClick={() => setOpen(true)}>
-        Show Alert
-      </Button>
+			{/* Test Alert Dialog */}
+			<Button variant='destructive' onClick={() => setOpen(true)}>
+				Show Alert
+			</Button>
 
-      {/* Alert Dialog */}
-      <AlertDialog open={open()} onOpenChange={setOpen}>
-        <AlertDialogContent>
-          <AlertDialogTitle>Alert Dialog</AlertDialogTitle>
-          <AlertDialogDescription>
-            An Alert Dialog enables assistive technologies and browsers to distinguish alert dialogs from
-            other dialogs so they have the option of giving alert dialogs special treatment, such as
-            playing a system alert sound.
-          </AlertDialogDescription>
-        </AlertDialogContent>
-      </AlertDialog>
-    </div>
-  );
+			{/* Alert Dialog */}
+			<AlertDialog open={open()} onOpenChange={setOpen}>
+				<AlertDialogContent>
+					<AlertDialogTitle>Alert Dialog</AlertDialogTitle>
+					<AlertDialogDescription>
+						An Alert Dialog enables assistive technologies and browsers to distinguish alert dialogs from other dialogs so they have the option of giving alert dialogs special treatment, such as playing a system alert
+						sound.
+					</AlertDialogDescription>
+				</AlertDialogContent>
+			</AlertDialog>
+		</div>
+	)
 }
