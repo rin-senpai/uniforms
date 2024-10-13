9a0a4db07b748943ac1e904470e8a2c988f724d3
camellionn <thanhtra2004@gmail.com>
2024-10-13 15:27:54 +1100

    Add in Alert Dialouge

--- client/src/components/Counter.tsx
+++ client/src/components/Counter.tsx
@@ -1,11 +1,34 @@
-import { createSignal } from 'solid-js'
-import { Button } from "~/components/ui/button"
+import { createSignal } from 'solid-js';
+import { Button } from "~/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from "~/components/ui/alert-dialog";
 
 export default function Counter() {
-	const [count, setCount] = createSignal(0)
-	return (
-		<Button variant="ghost" onClick={() => setCount(count() + 1)}>
-			Clicks: {count()}
-		</Button>
-	)
+  const [open, setOpen] = createSignal(false); // Signal to control AlertDialog open state
+
+  return (
+    <div>
+      {/* Test Alert Dialog */}
+      <Button variant="destructive" onClick={() => setOpen(true)}>
+        Show Alert
+      </Button>
+
+      {/* Alert Dialog */}
+      <AlertDialog open={open()} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogTitle>Alert Dialog</AlertDialogTitle>
+          <AlertDialogDescription>
+            An Alert Dialog enables assistive technologies and browsers to distinguish alert dialogs from
+            other dialogs so they have the option of giving alert dialogs special treatment, such as
+            playing a system alert sound.
+          </AlertDialogDescription>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
 }
