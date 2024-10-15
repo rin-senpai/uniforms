382a44be53b5ae7b87af46000fb06f562b92aba0
Raine Park <rainepark1@gmail.com>
2024-10-16 03:11:27 +1100

    Add errorCodes map

---
+++ server/src/errorCodes.ts
@@ -0,0 +1,9 @@
+const errorMap: Map<number, string> = new Map([
+	[400, "Bad Request"],
+	[401, "Unauthorized"],
+	[403, "Forbidden"],
+	[404, "Not Found"],
+	[500, "Internal Server Error"],
+	[502, "Bad Gateway"],
+	[503, "Service Unavailable"]
+]);
