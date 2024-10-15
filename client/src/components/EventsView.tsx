71b92b2fcc5d37928e3ba325f0b4215b20906ab3
raoli521 <chatiiercheese@gmail.com>
2024-10-15 21:32:58 +1100

    added breakpoints to eventview

--- client/src/components/EventsView.tsx
+++ client/src/components/EventsView.tsx
@@ -50,7 +50,7 @@ export default function EventsView(props: any) {
 
 	const [local, others] = splitProps(props, ['class'])
 	return (
-		<div class={cn('grid grid-cols-5 gap-4 m-4', local.class)} {...others}>
+		<div class={cn('grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 m-4', local.class)} {...others}>
 			<For each={eventsList()}>
 				{(item) => (
 					<Card class=''>
@@ -59,21 +59,21 @@ export default function EventsView(props: any) {
 								<img class='object-contain' src={item.bannerURI} />
 								<Show when={!compact()}>
 									<div class='absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-30'></div>
-									<CardTitle class='absolute inset-x-0 bottom-5 text-white text-lg'>{item.title}</CardTitle>
+									<CardTitle class='absolute inset-x-0 bottom-5 text-white text-sm xl:text-lg'>{item.title}</CardTitle>
 								</Show>
 							</div>
 						</CardHeader>
 
 						<Show when={!compact()}>
-							<CardDescription class='justify-self-center mx-5 mt-5 flex flex-row flex-wrap gap-4'>
-								<div class='flex flex-row gap-2 items-center'>
+							<CardDescription class='justify-center mx-5 mt-5 flex flex-row flex-wrap gap-4'>
+								<div class='flex flex-col md:flex-row gap-2 items-center'>
 									<Clock />
 									<p class='font-bold'>
 										{dateFormat(item.timeStart)} {dateFormatHour(item.timeStart, item.timeEnd)}
 									</p>
 								</div>
 
-								<div class='flex flex-row gap-2 items-center'>
+								<div class='flex flex-col md:flex-row gap-2 items-center'>
 									<Map />
 									<p class='font-bold'>{item.location}</p>
 								</div>
