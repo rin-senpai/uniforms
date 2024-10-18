4367070d42e57d8b0ee34283d52116af159af73e
Raine Park <rainepark1@gmail.com>
2024-10-18 18:53:14 +1100

    Fix alignment issues and remove old test components from home page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -28,14 +28,6 @@ export default function Home(props: any) {
 	return (
 		<main class='text-center mx-auto text-gray-700 p-10 flex-1 overflow-x-hidden'>
 			<h1 class='max-6-xs text-6xl text-sky-700 font-thin uppercase my-16'>Hello world!</h1>
-			<Counter />
-			<p class='my-4'>
-				<span>Home</span>
-				{' - '}
-				<A href='/about' class='text-sky-600 hover:underline'>
-					About Page
-				</A>
-			</p>
 
 			{/* Notifications Section */}
 			<Card class='w-full max-w-[1100px] mx-auto'>
@@ -63,7 +55,7 @@ export default function Home(props: any) {
 			{/* Managed Socs Section */}
 			<div class='flex items-start space-x-4 gap-28 mt-4 max-w-[1100px] mx-auto'>
 				<div class='flex flex-col items-center'>
-					<h2 class='text-xl font-bold mb-4'>Managed Socs</h2>
+					<h2 class='text-xl font-bold'>Managed Socs</h2>
 					<div class='flex flex-col max-w-md w-[250px] grid-cols-3 gap-6 mt-4 text-3xl'>
 						<Button class='w-full h-10'>DevSoc</Button>
 						<Button class='w-full'>CSESoc</Button>
@@ -72,19 +64,14 @@ export default function Home(props: any) {
 				</div>
 
 				<div class='flex flex-col justify-start'>
-					<h2 class='text-xl font-bold mb-4 justify-start'>Upcoming Events</h2>
-					<EventsView class='w-[1200px] gap-10' events={eventsListDefault} numberOfEvents={3} />
-				</div>
-
-				{/* QR Code test */}
-				<div>
-					<QRCodeButton link='https://rin.vin' />
+					<h2 class='text-xl font-bold text-left'>Upcoming Events</h2>
+					<EventsView class='w-[1200px] gap-10 mx-0' events={eventsListDefault} numberOfEvents={3} />
 				</div>
 			</div>
 
 			{/* Single Event Section */}
 			<div class='flex flex-col items-start mt-10 max-w-[1100px] mx-auto'>
-				<h2 class='text-3xl font-bold mb-4'>Spot Light</h2>
+				<h2 class='text-3xl font-bold mb-4'>Spotlight Event</h2>
 				<Card class='flex flex-row w-full p-6 border-pink-400'>
 					<img src={selectedEvent.bannerURI} alt={selectedEvent.title} class='rounded-lg mb-4 w-1/3 h-64 object-cover' />
 					<div class='w-2/3 ml-6'>
