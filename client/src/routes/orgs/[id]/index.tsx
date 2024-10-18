f757c3d0a9a0dcb84d72a35b7af334f4b4ee5206
raoli521 <chatiiercheese@gmail.com>
2024-10-18 22:10:58 +1100

    individual soc event stuff started

--- client/src/routes/orgs/[id]/index.tsx
+++ client/src/routes/orgs/[id]/index.tsx
@@ -1,40 +1,61 @@
 import SocietyView from '~/components/SocietyView'
 import { boulderingURI, skullimgURI } from '~/components/URItest'
+import { createQuery, QueryClient, QueryClientProvider } from '@tanstack/solid-query'
+import { SolidQueryDevtools } from '@tanstack/solid-query-devtools'
+import { Show, Suspense } from 'solid-js'
+import { useNavigate, useParams } from '@solidjs/router'
 
-import { Organisation } from '../../../../server/src/interface'
+const url = 'localhost'
+const dev_port = 60000
+const SERVER_URL = `${url}:${dev_port}`
 
-const societies: Organisation[] = [
-	{
-		id: 1,
-		name: 'AnimeUNSW',
-		description: 'anime is real',
-		avatarURI: boulderingURI,
-		bannerURI: null,
-		createdAt: new Date()
-	},
-	{
-		id: 2,
-		name: 'CSESoc',
-		description: 'computer science rules!!!!!!!!!1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1',
-		avatarURI: skullimgURI,
-		bannerURI: null,
-		createdAt: new Date()
-	},
-	{
-		id: 3,
-		name: 'DevSoc',
-		description: 'i love coding so much ahshdjadalsdjaksdkjs',
-		avatarURI: skullimgURI,
-		bannerURI: null,
-		createdAt: new Date()
-	}
-]
+const queryClient = new QueryClient()
 
-export default function Orgs() {
+export default function IndividualOrgs() {
 	return (
-		<div class='w-full m-5'>
-			<h1 class='text-center max-6-xs text-6xl text-sky-700 font-thin uppercase my-16'>Organisations</h1>
-			<SocietyView orgs={societies} />
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<IndividualOrgsQuery />
+			<SolidQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	)
+}
+
+
+function IndividualOrgsQuery() {
+	const navigate = useNavigate()
+	const params = useParams()
+	const query = createQuery(() => ({
+		queryKey: ['data'],
+		queryFn: async () => {
+			const response = await fetch(`http://${SERVER_URL}/orgs/${params.id}`, {
+				method: 'GET'
+			})
+
+			if (!response.ok) {
+				throw new Error(`Response status: ${response.status}`)
+			}
+
+			const body = await response.json()
+
+			return {
+				name: body.organisation.name,
+				description: body.organisation.description,
+				avatarURI: body.organisation.avatarURI,
+				bannerURI: body.organisation.bannerURI
+			}
+		},
+		refetchOnWindowFocus: true, // Refetch when window gains focus
+    	refetchOnMount: true, // Refetch when the component mounts
+	}))
+	return (
+		<Suspense fallback={'Loading...'}>
+			<div class='w-full m-5'>
+				<div class='*:rounded-lg relative'>
+					<img class='object-contain' src={query.data?.bannerURI} />
+					<div class='absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-30'></div>
+					<h2 class='absolute inset-x-0 bottom-5 text-white text-sm lg:text-lg'>{query.data?.name}</h2>
+				</div>
+			</div>
+		</Suspense>
 	)
 }
